b03131a967842df2e73c82cec2b9e9e1082a5758
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-13 15:33:59 +0800

    [CHORE]: logger: added prompt

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
 import chalk from 'chalk';
-import inquirer from 'inquirer';
+import { prompt as promptInput, rl } from './prompt';
+
+type Text = string | Error | Array<string | Error>;
+type Color = 'blueBright' | 'greenBright' | 'redBright';
 
 export function createConsoleLogger(descriptiveTitle: string) {
-    const curriedPrint = (emoji: string) => (color: 'blueBright' | 'greenBright' | 'redBright') => {
-        return (descriptiveTitle: string) => (text: string | Error | Array<string | Error>) => {
+    const curriedPrint = (emoji: string) => (color: Color) => {
+        return (descriptiveTitle: string) => (text: Text) => {
             const title = chalk[color].bold(`${emoji} [${descriptiveTitle}]`);
             const body = chalk.whiteBright.bgBlack(
                 (Array.isArray(text) ? text : [text]).map((_) => _.toString()).join(' '),
             );
             console.info('');
-            console.info(`${title} ${body}`);
+            return `${title} ${body}`;
         };
     };
 
-    const prompt = async (question: string) => {
-        const logger = curriedPrint('🖊')('blueBright')(descriptiveTitle);
-        logger(question);
-        const answer = await inquirer.prompt([
-            {
-                type: 'input',
-                name: ' ',
-            },
-        ]);
-        return answer[' '];
+    const log = (callback: (text: Text) => string) => (text: Text) => console.info(callback(text));
+
+    const prompt = (callback: (text: Text) => string) => async (text: Text) => {
+        const answer = await promptInput(callback(text));
+        rl.close();
+        return answer;
     };
 
     return {
-        info: curriedPrint('ℹ️')('blueBright')(descriptiveTitle),
-        task: curriedPrint('🛠')('greenBright')(descriptiveTitle),
-        error: curriedPrint('❌')('redBright')(descriptiveTitle),
-        prompt,
+        info: log(curriedPrint('ℹ️')('blueBright')(descriptiveTitle)),
+        task: log(curriedPrint('🛠')('greenBright')(descriptiveTitle)),
+        error: log(curriedPrint('❌')('redBright')(descriptiveTitle)),
+        prompt: prompt(curriedPrint('🖊')('blueBright')(descriptiveTitle)),
     };
 }
---
+++ src/prompt.ts
@@ -0,0 +1,8 @@
+import util from 'util';
+import readline from 'readline';
+
+export const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+export const prompt = util.promisify(rl.question).bind(rl);
