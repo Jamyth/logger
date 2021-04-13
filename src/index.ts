68b0685e77b6e7104b209e47284aa225ac765ef0
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-13 16:10:26 +0800

    [FIX]: logger: fix rl is not close

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk';
-import { prompt as promptInput, rl } from './prompt';
+import util from 'util';
+import readline from 'readline';
 
 type Text = string | Error | Array<string | Error>;
 type Color = 'blueBright' | 'greenBright' | 'redBright';
@@ -19,6 +20,12 @@ export function createConsoleLogger(descriptiveTitle: string) {
     const log = (callback: (text: Text) => string) => (text: Text) => console.info(callback(text));
 
     const prompt = (callback: (text: Text) => string) => async (text: Text) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+        const promptInput = (util.promisify(rl.question).bind(rl) as unknown) as (question: string) => string;
+
         const answer = await promptInput(callback(text));
         rl.close();
         return answer;
--- src/prompt.ts
+++
@@ -1,8 +0,0 @@
-import util from 'util';
-import readline from 'readline';
-
-export const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-export const prompt = (util.promisify(rl.question).bind(rl) as unknown) as (question: string) => string;
